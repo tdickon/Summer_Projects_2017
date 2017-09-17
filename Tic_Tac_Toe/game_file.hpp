b1947c307114c53bcf08a91fbd98d0d792ecf0f3
Tyler Dickon <tdickon@kent.edu>
2017-09-16 23:21:26 -0400

    Update game_file.hpp

--- Tic_Tac_Toe/game_file.hpp
+++ Tic_Tac_Toe/game_file.hpp
@@ -1,48 +1,90 @@
-#ifndef  GAME_FILE_HPP
-#define GAME_FILE_HPP
-
-#include <iostream>
+////////////////////////////////////////////////////////////////////////////////////
+#ifndef GAME_HPP
+#define GAME_HPP
+//---------------------------------------------------------------------------------
 #include<string>
+#include<iostream>
 #include<vector>
-
-class player
+#include <ctime>
+///////////////////////////////////////////////////////////////////////////////////
+//=================================================================================
+//Character Domain
+//=================================================================================
+//The character domain holds all of the classes
+//that deal with characters (players).
+//---------------------------------------------------------------------------------
+//1.)Character - Base Class that holds all of the basic information for the player. 
+class character
 {
-private:
-	std::string player_name;
-	char player_icon;
+protected:
+	std::string character_name;
+	char character_icon;
+	int money_won;
 	int win_count;
 public:
 	//Constructors
-	player();
-	player(std::string, char);
+	character(); 
+	character(std::string, char);
 	//Mutators
 	void add_win() { this->win_count = win_count + 1; }
 	//ACCESSORS
 	void name_output();
 	void icon_output();
 	void win_count_output();
-	char return_user_icon();
-	//OPERATOR OVERLOADING
-	void swap(player &);
-	void operator=(player);
+	char return_char_icon();
 };
-
+//---------------------------------------------------------------------------------
+//2.)player_character is a subclass that deals with actual player characters. 
+class player_character : character
+{
+public:
+	int get_player_input();
+};
+//---------------------------------------------------------------------------------
+//3.)machine_character is a subclass that deals with the machine posing as a player.
+class machine_character : character
+{
+public:
+	int get_machine_input();
+};
+//---------------------------------------------------------------------------------
+//=================================================================================
+//End of Character Domain
+//=================================================================================
+///////////////////////////////////////////////////////////////////////////////////
+//=================================================================================
+//Board Domain
+//=================================================================================
+//The Board domain holds all of the classes that deal with the visual rep. of the
+//board on the console. 
+//---------------------------------------------------------------------------------
+//1.)Board is the class that holds all of the functionality that draws up the board.
 class board
 {
 private:
-	char board_boxes[10];
+	char board_cells[10];
 public:
 	board();
-	void create_box(player &, const int);
-	void board_generation(player & player_1, player & player_2);
+	void create_cell(player_character &, const int);
+	void create_cell (machine_character &, const int);
+	void board_generation (player_character & player_1, player_character & player_2);
+	void board_generation(player_character & player_1, machine_character & player_2);
 	void board_reset();
 };
-
-class game
+//---------------------------------------------------------------------------------
+//=================================================================================
+//End of the Board Domain
+//=================================================================================
+///////////////////////////////////////////////////////////////////////////////////
+//=================================================================================
+//Game Domain
+//=================================================================================
+//The game domain holds all of the classes that deal with the game functioning. 
+//---------------------------------------------------------------------------------
+//1.)Base_Game is the parent class that holds all of the basic game functions.
+class base_game
 {
-private:
-	player player_1;
-	player player_2;
+protected:
 	std::vector<int> player_1_moves;
 	std::vector<int> player_2_moves;
 	int current_move;
@@ -51,27 +93,59 @@ private:
 	bool game_complete;
 	bool player_1_turn;
 public:
-	game();
+	base_game();
 	void run_game();
 	void reset();
 	void check_game(); //After every move, the game will check to see if the game has been completed. It will also assign the next player to go.
 	void player_move(); //Player move will be made after every check, they will type a number in from 1 to 9 to choose their space.
 	void play(); //Play run's the game each frame, and runs in the run_game file;
 	void board_refresh();
-	bool box_check(const int &);
+	bool cell_check(const int &);
 	void new_game(); //At completion of the game, the player will be asked if they want to start a new game.
 };
-
-const int winning_combos[8][3] =
+//---------------------------------------------------------------------------------
+//2.)pvp deals with the functionality between two player characters. 
+class pvp : base_game
 {
-	{ 1,2,3 }, //0
-	{ 4,5,6 }, //1
-	{ 7,8,9 }, //2
-	{ 1,4,7 }, //3
-	{ 2,5,8 }, //4
-	{ 3,6,9 }, //5
-	{ 1,5,9 }, //6
-	{ 7,5,3 } //7
+protected:
+	player_character player_1;
+	player_character player_2;
+public:
+	pvp();
 };
-
+//---------------------------------------------------------------------------------
+//3.) pvm deals with player vs machine functionality. 
+class pvm : base_game
+{
+protected:
+	player_character player_1;
+	machine_character npc;
+public:
+	pvm();
+	void player_move();	
+};
+//---------------------------------------------------------------------------------
+//=================================================================================
+//End of Game Domain
+//=================================================================================
+///////////////////////////////////////////////////////////////////////////////////
+//=================================================================================
+//Execution Domain
+//=================================================================================
+//The execution domain deals with the class that executes the application.
+//---------------------------------------------------------------------------------
+//1.)Execute deals with executing the program. 
+class execute
+{
+private: 
+	bool game_type;
+public:
+	execute();
+};
+//---------------------------------------------------------------------------------
+//=================================================================================
+//End of Execution Domain
+//=================================================================================
+///////////////////////////////////////////////////////////////////////////////////
 #endif
+///////////////////////////////////////////////////////////////////////////////////
