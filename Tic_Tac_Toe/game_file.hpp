1e30df03514d913b6408c09f8965fb4a12ff050a
Tyler Dickon <tdickon@kent.edu>
2017-09-18 14:54:14 -0400

    Update game_file.hpp

--- Tic_Tac_Toe/game_file.hpp
+++ Tic_Tac_Toe/game_file.hpp
@@ -1,11 +1,10 @@
 ////////////////////////////////////////////////////////////////////////////////////
-#ifndef GAME_HPP
-#define GAME_HPP
+#ifndef GAME_FILE_HPP
+#define GAME_FILE_HPP
 //---------------------------------------------------------------------------------
 #include<string>
 #include<iostream>
 #include<vector>
-#include <ctime>
 ///////////////////////////////////////////////////////////////////////////////////
 //=================================================================================
 //Character Domain
@@ -23,29 +22,62 @@ protected:
 	int win_count;
 public:
 	//Constructors
-	character(); 
+	character();
 	character(std::string, char);
 	//Mutators
-	void add_win() { this->win_count = win_count + 1; }
+	void add_win();
 	//ACCESSORS
 	void name_output();
 	void icon_output();
 	void win_count_output();
 	char return_char_icon();
+	void return_money_won() { std::cout << "You won " << this->money_won << ", congrats!"; }
 };
 //---------------------------------------------------------------------------------
 //2.)player_character is a subclass that deals with actual player characters. 
-class player_character : character
+class player_character :  character
 {
 public:
+	//Constructors
+	player_character() { character::character(); }
+	player_character(std::string user_name, char icon) { character::character(user_name, icon); }
+	//Getters
 	int get_player_input();
+	//Mutators
+	void add_win() { character::add_win(); }
+	//ACCESSORS
+	void name_output() { character::name_output(); }
+	void icon_output() { character::icon_output(); }
+	void win_count_output() { character::win_count_output(); }
+	char return_char_icon() { return character_icon; }
+	void operator=(player_character right_hand_side);
+	void set_name(std::string user_name) { character_name = user_name; }
+	void set_icon(char user_icon) { character_icon = user_icon; }
+	int return_win_count() { return this->win_count; }
+	void return_money_won() { character::return_money_won(); }
 };
 //---------------------------------------------------------------------------------
 //3.)machine_character is a subclass that deals with the machine posing as a player.
 class machine_character : character
 {
 public:
+	//Constructors
+	machine_character() { character::character(); }
+	machine_character(std::string user_name, char icon) { character::character(user_name, icon); }
+	//Getters
 	int get_machine_input();
+	//Mutators
+	void add_win() { character::add_win(); }
+	//ACCESSORS
+	void name_output() { character::name_output(); }
+	void icon_output() { character::icon_output(); }
+	void win_count_output() { character::win_count_output(); }
+	char return_char_icon() { return character_icon; }
+	void operator=(machine_character right_hand_side);
+	void set_name(std::string user_name) { character_name = user_name; }
+	void set_icon(char user_icon) { character_icon = user_icon; }
+	int return_win_count() { return this->win_count; }
+	void return_money_won() { character::return_money_won(); }
 };
 //---------------------------------------------------------------------------------
 //=================================================================================
@@ -66,8 +98,8 @@ private:
 public:
 	board();
 	void create_cell(player_character &, const int);
-	void create_cell (machine_character &, const int);
-	void board_generation (player_character & player_1, player_character & player_2);
+	void create_cell(machine_character &, const int);
+	void board_generation(player_character & player_1, player_character & player_2);
 	void board_generation(player_character & player_1, machine_character & player_2);
 	void board_reset();
 };
@@ -92,16 +124,24 @@ protected:
 	board game_board;
 	bool game_complete;
 	bool player_1_turn;
+	std::string get_user_name();
+	void reset_move_set(std::vector<int> &);
+	bool array_check(std::vector<int>);
+	const int winning_combos[8][3] =
+	{
+		{ 1, 2, 3 }, //0
+		{ 4, 5, 6 }, //1
+		{ 7, 8, 9 }, //2
+		{ 1, 4, 7 }, //3
+		{ 2, 5, 8 }, //4
+		{ 3, 6, 9 }, //5
+		{ 1, 5, 9 }, //6
+		{ 7, 5, 3 } //7
+	};
 public:
 	base_game();
-	void run_game();
 	void reset();
-	void check_game(); //After every move, the game will check to see if the game has been completed. It will also assign the next player to go.
-	void player_move(); //Player move will be made after every check, they will type a number in from 1 to 9 to choose their space.
-	void play(); //Play run's the game each frame, and runs in the run_game file;
-	void board_refresh();
 	bool cell_check(const int &);
-	void new_game(); //At completion of the game, the player will be asked if they want to start a new game.
 };
 //---------------------------------------------------------------------------------
 //2.)pvp deals with the functionality between two player characters. 
@@ -111,7 +151,13 @@ protected:
 	player_character player_1;
 	player_character player_2;
 public:
-	pvp();
+	pvp() { this->run_game(); }
+	void run_game();
+	void check_game(); //After every move, the game will check to see if the game has been completed. It will also assign the next player to go.
+	void player_move(); //Player move will be made after every check, they will type a number in from 1 to 9 to choose their space.
+	void play(); //Play run's the game each frame, and runs in the run_game file;
+	void board_refresh();
+	void new_game(); //At completion of the game, the player will be asked if they want to start a new game.
 };
 //---------------------------------------------------------------------------------
 //3.) pvm deals with player vs machine functionality. 
@@ -119,10 +165,15 @@ class pvm : base_game
 {
 protected:
 	player_character player_1;
-	machine_character npc;
+	machine_character player_2;
 public:
-	pvm();
-	void player_move();	
+	pvm() { this->run_game(); }
+	void run_game();
+	void check_game(); //After every move, the game will check to see if the game has been completed. It will also assign the next player to go.
+	void player_move(); //Player move will be made after every check, they will type a number in from 1 to 9 to choose their space.
+	void play(); //Play run's the game each frame, and runs in the run_game file;
+	void board_refresh();
+	void new_game(); //At completion of the game, the player will be asked if they want to start a new game.
 };
 //---------------------------------------------------------------------------------
 //=================================================================================
@@ -137,14 +188,25 @@ public:
 //1.)Execute deals with executing the program. 
 class execute
 {
-private: 
-	bool game_type;
+private:
+	int game_type;
+	int return_game_type();
 public:
 	execute();
 };
 //---------------------------------------------------------------------------------
 //=================================================================================
 //End of Execution Domain
+//=================================================================================
+///////////////////////////////////////////////////////////////////////////////////
+//=================================================================================
+//Free Functions
+//=================================================================================
+void system_exe(); //executes the game
+void refresh_screen();
+//---------------------------------------------------------------------------------
+//=================================================================================
+//End of Free Functions
 //=================================================================================
 ///////////////////////////////////////////////////////////////////////////////////
 #endif
